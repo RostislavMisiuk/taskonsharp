cbf6cc5e0041643acb6fe8c3bdebe1b75c242ca8
RostislavMisyk <rostislav.misyk@gmail.com>
2021-11-05 17:16:17 +0300

    Create Program111.cs

---
+++ 30.09/Program111.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Fraction
+{
+    public int num;
+    public int den;
+
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.", nameof(denominator));
+        }
+        num = numerator;
+        den = denominator;
+    }
+
+    public static Fraction operator +(Fraction a) => a;
+    public static Fraction operator -(Fraction a) => new Fraction(-a.num, a.den);
+
+    public static Fraction operator +(Fraction a, Fraction b)
+        => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
+
+    public static Fraction operator -(Fraction a, Fraction b)
+        => a + (-b);
+
+    public static Fraction operator *(Fraction a, Fraction b)
+        => new Fraction(a.num * b.num, a.den * b.den);
+
+    public static Fraction operator /(Fraction a, Fraction b)
+    {
+        if (b.num == 0)
+        {
+            throw new DivideByZeroException();
+        }
+        return new Fraction(a.num * b.den, a.den * b.num);
+    }
+
+    public override string ToString() => $"{num} / {den}";
+}
+
+public static class OperatorOverloading
+{
+    public static void Main()
+    {
+        var a = new Fraction(7, 2);
+        var b = new Fraction(3, 5);
+        Console.WriteLine(-a);
+        Console.WriteLine(a + b);
+        Console.WriteLine(a - b);
+        Console.WriteLine(a * b);
+        Console.WriteLine(a / b);
+    }
+}
