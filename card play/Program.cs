0b4e02d95ca0dd486477ca2f43968bf539800323
RostislavMisyk <rostislav.misyk@gmail.com>
2021-11-10 18:38:57 +0300

    task

---
+++ card play/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game game = new Game(6);
+            while (game.playersTurn())
+            {
+
+            }
+        }
+    }
+
+    
+}
